acd6cd688ffa0ac0068b950a635ead09da9b24f6
botmenoff <marvenferry60@gmail.com>
2021-05-07 17:04:35 +0200

    JUST A NORMAL COMMIT

--- js/codigo.js
+++ js/codigo.js
@@ -3,6 +3,6 @@ function language() {
     if (idioma == 'ES') {
         window.location.href = '../index.html'
     } else {
-        window.location.href = '../en/index.html'
+        window.location.href = './en/index.html'
     }
 }
